3e9ac4dbefcf7d9dad464e29fdd2262eee1cf82e
Quentin Husky <qthusk@gmail.com>
2015-05-29 21:36:55 +0200

    Initial commit of the f-chat.io package

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+// Expose FChatClient class
+exports.FChatClient = require('./lib/chat-client');
+
+// Expose FListClient class
+exports.FListClient = require('./lib/json-endpoint');
